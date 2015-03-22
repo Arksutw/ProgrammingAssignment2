5303b787cdd44854677cd57c14fe5f63012570e9
Arksutw <arksutw@gmail.com>
2015-03-23 01:07:39 +0800

    ProgrammingAssignment2 done

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,18 @@
 
 ## Write a short comment describing this function
 ## Made By Ark
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mx = matrix()) {
+  
+  inverse <- NULL
+  set <- function(x) {
+          mx <<- x
+          inverse <<- NULL
+  }
+  
+  get <- function() mx
+  setinv <- function(inv) inverse <<- inv;
+  getinv <- function() inverse;
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
@@ -12,4 +22,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  inverse <- mx$getinv()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+   
+  data <- mx$get()
+  invserse <- solve(data, ...)
+  mx$setinv(inverse) 
+  inverse
 }
+
